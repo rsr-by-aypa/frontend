f1a2dbe6a674dd0fab76a972efd1f5e322dbca45
Grischakrombacha <grischa.storch@gmail.com>
2024-07-17 08:31:05 +0200

    ShoppingCart wird gefetcht

--- src/components/content/basket/Basket.jsx
+++ src/components/content/basket/Basket.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Basket.css';
 import { useNavigate } from 'react-router-dom';
 
-const Basket = ({ items, total, shippingCost }) => {
+const Basket = () => {
+
+    const [shoppingCart, setShoppingCart] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+      }, []);
+
+    const fetchData = async () => {
+        try {
+          try {
+            await keycloak.init({ onLoad: 'login-required' });
+          } catch (error) {
+            console.log("Keycloak Instance has already been initialized");
+          }
+          const token = keycloak.token;
+
+          const response = await fetch('/api/shopping-cart', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          if (!response.ok) {
+            if (response.status === 401) {
+
+              keycloak.login(
+                {
+                  redirectUri: "/basket", // Hier die gewünschte redirect_uri angeben
+                }
+              );
+              return;
+            }
+            throw new Error('Network response was not ok');
+          }
+
+          const data = await response.json();
+          setShoppingCart(data);
+        } catch (error) {
+          console.error('Error fetching items:', error);
+        }
+      };
 
     const navigate = useNavigate();
 
@@ -13,7 +53,7 @@ const Basket = ({ items, total, shippingCost }) => {
           <div className="items-list">
             <h3>Artikel im Warenkorb:</h3>
             <ul>
-              <p>Liste der Items</p>
+              <p>{shoppingCart}</p>
             </ul>
           </div>
           <div className="basket-summary">
